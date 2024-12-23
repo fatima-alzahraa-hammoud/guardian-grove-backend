8f5c2fd0bcf687c06109cb15b718acc709bdb554
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-23 15:37:43 +0200

    Add Express Type to app

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import express from "express";
+import express, {Express} from "express";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const app = express();
+const app: Express= express();
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
