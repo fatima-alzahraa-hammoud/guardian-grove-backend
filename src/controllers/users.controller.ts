191a91b2788b5705dcbb5df25219d8ae7729b9e6
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-23 22:21:21 +0200

    Add createUser API

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -22,3 +22,14 @@ export const getUserById = async (req: Request, res: Response): Promise<void> =>
         res.status(500).json({message: "Error retrieving user", error});
     }
 };
+
+export const createUser = async (req: Request, res: Response): Promise<void> => {
+    try{
+        const data = req.body;
+        const user = await User.create({...data});
+
+        res.status(201).json(user);
+    }catch(error){
+        res.status(500).json({message: "An error occurred while adding the user.", error});
+    }
+}
