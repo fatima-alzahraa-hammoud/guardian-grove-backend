a41ed0e20b6e9096f5df16bc7a7815b43a4da7b6
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-23 15:20:59 +0200

    Configure dotenv for Environment Variables

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 
-app.listen(8080, () =>{
+app.listen(port, () =>{
     console.log("server is running on port 8080");
 });
