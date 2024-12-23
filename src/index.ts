8aae35f7929ae9d1be752a2cf169127d050a253c
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-23 16:05:39 +0200

    Fix Port Data Type

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import connectToDatabase from "./db/connection";
 dotenv.config();
 
 const app: Express= express();
-const port = process.env.PORT || 8080;
+const port: number = parseInt(process.env.PORT || "8080");
 
 app.use(express.json());
 
