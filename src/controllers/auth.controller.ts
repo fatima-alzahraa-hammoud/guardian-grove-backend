0af0ba1873000fc86b0d185c20e3f41164bb3d52
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-24 22:53:59 +0200

    Add register API

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,7 +2,9 @@ import { Response, Request } from "express";
 import bcrypt from "bcrypt";
 import { throwError } from "../utils/error";
 import { User } from "../models/user.model";
+import { createUser } from "./users.controller";
 
+// login
 export const login = async ( req: Request, res: Response) : Promise<void> => {
 
     try{
@@ -35,3 +37,31 @@ export const login = async ( req: Request, res: Response) : Promise<void> => {
         throwError({ message: "Something went wrong while logging in.", res, status: 500});
     }
 }
+
+// register
+export const register = async (req: Request, res: Response) : Promise<void> => {
+    try{
+        const data = req.body;
+        const { name, email, password, birthday, gender, role, avatar, interests } = data;
+        
+        // verify all fields are filled
+        if (!name || !email || !password || !birthday || !gender || !role || !avatar || !interests) {
+            throwError({ message: "All required fields must be filled.", res, status: 400});
+            return;
+        }
+
+        const user = await User.findOne({
+            email: email, 
+        });
+
+        if(user){
+            throwError({ message: "Family with this email exists", res, status: 400 });
+            return;
+        }
+        
+        const newUser = await createUser(req, res);
+
+    }catch(error){
+        throwError({ message: "Something went wrong while registering.", res, status: 500});
+    }
+}  
