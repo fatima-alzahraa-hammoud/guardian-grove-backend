211e766bdacfacf409de28760b444c5ad976d69c
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-25 22:53:24 +0200

    Add updateAdventure router

--- src/routes/adventure.routes.ts
+++ src/routes/adventure.routes.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
-import { createAdventure, getAdventureById, getAllAdventures } from "../controllers/adventure.controller";
+import { createAdventure, getAdventureById, getAllAdventures, updateAdventure } from "../controllers/adventure.controller";
 
 const router =  express.Router();
 
 router.post("/", createAdventure);
 router.get("/", authMiddleware, getAllAdventures);
 router.get("/adventure", authMiddleware, getAdventureById);
+router.put("/", authMiddleware, updateAdventure);
 
 
 export default router;
