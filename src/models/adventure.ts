4fb634932cf0f8c1bc2f5233ca014df9eeae248b
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-25 19:47:07 +0200

    Add AdventureSchema

--- src/models/adventure.ts
+++ src/models/adventure.ts
@@ -1,10 +1,30 @@
 import { Schema } from "mongoose";
 import { IChallenge } from "../interfaces/IChallenge";
+import { IAdventure } from "../interfaces/IAdventure";
 
 const challengeSchema = new Schema<IChallenge>({
     title: { type: String, required: true },
     content: { type: String, required: true },
-    starsReward: { type: Number, required: true, default: 10 },
-    coinsReward: { type: Number, required: true, default: 10 },
+    starsReward: { type: Number, required: true, default: 2 },
+    coinsReward: { type: Number, required: true, default: 1 },
     isCompleted: { type: Boolean, required: true, default: false },
 });
+
+const adventureSchema = new Schema<IAdventure>({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    starsReward: { type: Number, required: true, default: 10 },
+    coinsReward: { type: Number, required: true, default: 5 },
+    createdAt: { type: Date, default: Date.now },
+
+    // Automatically calculate due date (24 hours after creation)
+    dueDate: { 
+        type: Date,
+        required: true,
+        default: () => new Date(Date.now() + 24 * 60 * 60 * 1000)
+    },
+
+    challenges: { type: [challengeSchema], required: true, default: [] },
+});
+
+export { adventureSchema };
