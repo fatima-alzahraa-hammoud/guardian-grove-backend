8b860abf215949d790db5b5727ba95a365972788
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-25 22:57:29 +0200

    Add deleteAdventure router

--- src/routes/adventure.routes.ts
+++ src/routes/adventure.routes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
-import { createAdventure, getAdventureById, getAllAdventures, updateAdventure } from "../controllers/adventure.controller";
+import { createAdventure, deleteAdventure, getAdventureById, getAllAdventures, updateAdventure } from "../controllers/adventure.controller";
 
 const router =  express.Router();
 
@@ -8,6 +8,7 @@ router.post("/", createAdventure);
 router.get("/", authMiddleware, getAllAdventures);
 router.get("/adventure", authMiddleware, getAdventureById);
 router.put("/", authMiddleware, updateAdventure);
+router.delete("/", authMiddleware, deleteAdventure);
 
 
 export default router;
