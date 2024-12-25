83a017a847357f468b09d241a74d62fa089a1b3f
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-25 22:04:04 +0200

    Add getAllAdventures API

--- src/controllers/adventure.controller.ts
+++ src/controllers/adventure.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { Adventure } from "../models/adventure.model";
 import { IAdventure } from '../interfaces/IAdventure';
 import { throwError } from '../utils/error';
+import { CustomRequest } from '../interfaces/customRequest';
 
 // API to create new adventure
 export const createAdventure = async (req: Request, res: Response):Promise<void> => {
@@ -23,10 +24,25 @@ export const createAdventure = async (req: Request, res: Response):Promise<void>
             challenges: [], 
         });
 
-        const adventure = await newAdventure.save();
+        await newAdventure.save();
+
         res.status(201).json({message: "Adventure created successfully", adventure: newAdventure});
     } catch (error) {
         throwError({ message: "An unknown error occurred.", res, status: 500 });
     }
 };
 
+// Get all adventures
+export const getAllAdventures = async (req: Request, res: Response) => {
+    try {
+        const adventures = await Adventure.find();
+
+        if(!adventures){
+            throwError({message: "No adventures found", res, status: 400});
+        }
+
+        res.status(200).json({message: "Getting all adventures Successfully", adventures});
+    } catch (error) {
+        throwError({ message: "An unknown error occurred while getting all adventures.", res, status: 500 });
+    }
+};
