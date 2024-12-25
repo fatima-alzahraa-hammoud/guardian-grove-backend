a692134185f9776a4f634b9d5c7e0e917eaaf83a
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-25 21:47:19 +0200

    Add createAdventure router and Fix API

--- src/controllers/adventure.controller.ts
+++ src/controllers/adventure.controller.ts
@@ -1,14 +1,19 @@
 import { Request, Response } from 'express';
 import { Adventure } from "../models/adventure.model";
 import { IAdventure } from '../interfaces/IAdventure';
+import { throwError } from '../utils/error';
 
 // API to create new adventure
-export const createAdventure = async (req: Request, res: Response) => {
+export const createAdventure = async (req: Request, res: Response):Promise<void> => {
     try {
 
         const data = req.body;
 
         const { title, description, starsReward, coinsReward } = data;
+        if (!title || !description) {
+            throwError({ message: "All required fields must be filled.", res, status: 400});
+            return;
+        }
 
         const newAdventure: IAdventure = new Adventure({
             title,
@@ -21,7 +26,7 @@ export const createAdventure = async (req: Request, res: Response) => {
         const adventure = await newAdventure.save();
         res.status(201).json({message: "Adventure created successfully", adventure: newAdventure});
     } catch (error) {
-
+        throwError({ message: "An unknown error occurred.", res, status: 500 });
     }
 };
 
--- src/routes/adventure.routes.ts
+++ src/routes/adventure.routes.ts
@@ -1,6 +1,9 @@
 import express from "express";
+import { authMiddleware } from "../middlewares/auth.middleware";
+import { createAdventure } from "../controllers/adventure.controller";
 
 const router =  express.Router();
 
+router.post("/", createAdventure);
 
 export default router;
