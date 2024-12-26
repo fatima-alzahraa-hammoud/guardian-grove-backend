0ae7decab6fc33cacbf1744bfbe42cbe1896c787
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-26 16:12:27 +0200

    Add IStore interface

---
+++ src/interfaces/IStore.ts
@@ -0,0 +1,9 @@
+import { Document } from "mongoose";
+
+export interface IStore extends Document {
+    name: string;
+    description: string;
+    price: number;
+    image: string;
+    type: string;
+  }
