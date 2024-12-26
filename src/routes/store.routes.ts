bb4c9c466b669987e150efc8ac9d8c898b687576
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-26 18:31:05 +0200

    Add getStoreItems router

--- src/routes/store.routes.ts
+++ src/routes/store.routes.ts
@@ -1,5 +1,9 @@
 import express from "express";
+import { authMiddleware } from "../middlewares/auth.middleware";
+import { getStoreItems } from "../controllers/store.controller";
 
 const router =  express.Router();
+
+router.get("/", authMiddleware, getStoreItems);
 
 export default router;
