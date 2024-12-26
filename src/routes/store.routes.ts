061ccce6995b0295c7270cae94b325db6414be3f
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-26 19:39:49 +0200

    Add updateItem router

--- src/routes/store.routes.ts
+++ src/routes/store.routes.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
-import { createItem, deleteItem, getStoreItems } from "../controllers/store.controller";
+import { createItem, deleteItem, getStoreItems, updateItem } from "../controllers/store.controller";
 
 const router =  express.Router();
 
 router.get("/", authMiddleware, getStoreItems);
 router.post("/", authMiddleware, createItem);
 router.delete("/", authMiddleware, deleteItem);
+router.put("/", authMiddleware, updateItem);
 
 
 export default router;
