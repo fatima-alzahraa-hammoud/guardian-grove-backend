8c200cca162cbbffc54b775431d1c74d08d628cd
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-27 00:33:46 +0200

    Add adminMiddleware to create and update notications routers

--- src/routes/notification.routes.ts
+++ src/routes/notification.routes.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
 import { createNotification, deleteNotification, getNotifications, updateNotification } from "../controllers/notification.controller";
+import { adminMiddleware } from "../middlewares/adminMiddleware";
 
 const router =  express.Router();
 
 router.get("/", authMiddleware, getNotifications);
-router.post("/", authMiddleware, createNotification);
+router.post("/", authMiddleware, adminMiddleware, createNotification);
 router.delete("/", authMiddleware, deleteNotification);
-router.put("/", authMiddleware, updateNotification);
+router.put("/", authMiddleware, adminMiddleware, updateNotification);
 
 export default router;
