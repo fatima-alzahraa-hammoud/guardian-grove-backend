fb86471989213363143e8d9470a3e82b975d0f06
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-26 19:53:12 +0200

    Add buyItem router

--- src/routes/store.routes.ts
+++ src/routes/store.routes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
-import { createItem, deleteItem, getStoreItems, updateItem } from "../controllers/store.controller";
+import { buyItem, createItem, deleteItem, getStoreItems, updateItem } from "../controllers/store.controller";
 
 const router =  express.Router();
 
@@ -8,6 +8,8 @@ router.get("/", authMiddleware, getStoreItems);
 router.post("/", authMiddleware, createItem);
 router.delete("/", authMiddleware, deleteItem);
 router.put("/", authMiddleware, updateItem);
+router.post("/buy", authMiddleware, buyItem);
+
 
 
 export default router;
