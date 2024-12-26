a5fd69289249d5d3bf64e9f8ab21becb43d27d65
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-26 18:57:12 +0200

    Add createItem router

--- src/routes/store.routes.ts
+++ src/routes/store.routes.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
-import {getStoreItems } from "../controllers/store.controller";
+import { createItem, getStoreItems } from "../controllers/store.controller";
 
 const router =  express.Router();
 
 router.get("/", authMiddleware, getStoreItems);
+router.post("/", authMiddleware, createItem);
+
 
 export default router;
