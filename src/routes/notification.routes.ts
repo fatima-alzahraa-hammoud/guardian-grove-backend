4183ef5ce370df5aca2a488fdf50cec938cf07ff
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-26 22:41:37 +0200

    Add getNotifications router

--- src/routes/notification.routes.ts
+++ src/routes/notification.routes.ts
@@ -1,5 +1,9 @@
 import express from "express";
+import { authMiddleware } from "../middlewares/auth.middleware";
+import { getNotifications } from "../controllers/notification.controller";
 
 const router =  express.Router();
+
+router.get("/", authMiddleware, getNotifications);
 
 export default router;
