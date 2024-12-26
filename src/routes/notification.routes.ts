247f4388cdee884f8646bae2236ba017a9402f23
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-26 23:39:00 +0200

    Add deleteNotification router

--- src/routes/notification.routes.ts
+++ src/routes/notification.routes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
-import { createNotification, getNotifications } from "../controllers/notification.controller";
+import { createNotification, deleteNotification, getNotifications } from "../controllers/notification.controller";
 
 const router =  express.Router();
 
 router.get("/", authMiddleware, getNotifications);
 router.post("/", authMiddleware, createNotification);
+router.delete("/", authMiddleware, deleteNotification);
 
 export default router;
