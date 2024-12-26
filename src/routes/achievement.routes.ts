d73367baeb528071a1e0a666a7174c7b62a2a03b
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-26 15:16:18 +0200

    Add deleteAchievement router

--- src/routes/achievement.routes.ts
+++ src/routes/achievement.routes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
-import { createAchievement, getAllAchievements, getLockedAchievements, unlockAchievement, updateAchievement } from "../controllers/achievement.controller";
+import { createAchievement, deleteAchievement, getAllAchievements, getLockedAchievements, unlockAchievement, updateAchievement } from "../controllers/achievement.controller";
 import { adminMiddleware } from "../middlewares/adminMiddleware";
 
 const router =  express.Router();
@@ -11,6 +11,6 @@ router.get("/unlocked", authMiddleware, getLockedAchievements);
 router.post("/unlock", authMiddleware, unlockAchievement);
 router.post("/", authMiddleware, createAchievement);
 router.put("/", authMiddleware, adminMiddleware, updateAchievement);
-
+router.delete("/", authMiddleware, adminMiddleware, deleteAchievement);
 
 export default router;
