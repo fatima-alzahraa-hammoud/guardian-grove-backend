ca536c598cc4049e11c319654ee66be5590709df
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-26 13:18:39 +0200

    Add getAllAchievements router

--- src/routes/achievement.routes.ts
+++ src/routes/achievement.routes.ts
@@ -1,5 +1,9 @@
 import express from "express";
+import { authMiddleware } from "../middlewares/auth.middleware";
+import { getAllAchievements } from "../controllers/achievement.controller";
 
 const router =  express.Router();
+
+router.get("/", authMiddleware, getAllAchievements);
 
 export default router;
