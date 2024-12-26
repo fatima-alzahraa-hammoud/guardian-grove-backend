f259b4ca93486ce951d6a91384062eed1339ebeb
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-26 15:06:42 +0200

    Add createAchievement router

--- src/routes/achievement.routes.ts
+++ src/routes/achievement.routes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
-import { getAllAchievements, getLockedAchievements, unlockAchievement } from "../controllers/achievement.controller";
+import { createAchievement, getAllAchievements, getLockedAchievements, unlockAchievement } from "../controllers/achievement.controller";
 
 const router =  express.Router();
 
@@ -8,5 +8,7 @@ router.get("/", authMiddleware, getAllAchievements);
 router.get("/locked", authMiddleware, getLockedAchievements);
 router.get("/unlocked", authMiddleware, getLockedAchievements);
 router.post("/unlock", authMiddleware, unlockAchievement);
+router.post("/", authMiddleware, createAchievement);
+
 
 export default router;
