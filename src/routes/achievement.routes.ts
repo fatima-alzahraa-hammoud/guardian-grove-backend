12a740647dfee066fb889c785b6d634c02ab972e
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-26 13:29:07 +0200

    Add getLockedAchievements router

--- src/routes/achievement.routes.ts
+++ src/routes/achievement.routes.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
-import { getAllAchievements } from "../controllers/achievement.controller";
+import { getAllAchievements, getLockedAchievements } from "../controllers/achievement.controller";
 
 const router =  express.Router();
 
 router.get("/", authMiddleware, getAllAchievements);
+router.get("/locked", authMiddleware, getLockedAchievements);
 
 export default router;
