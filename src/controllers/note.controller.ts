bece7554931991f6a7e98b85a8f95306785eaf53
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-27 02:19:04 +0200

    Add getNotes API

--- src/controllers/note.controller.ts
+++ src/controllers/note.controller.ts
@@ -50,3 +50,34 @@ export const createNote = async (req: CustomRequest, res: Response) => {
         return throwError({ message: "Error creating note", res, status: 500 });
     }
 };
+
+//API to get notes of the user/users
+export const getNotes = async (req: Request, res: Response) => {
+    try {
+        const userId = req.body;
+        if (userId) {
+            const user = await User.findById(userId);
+
+            if (!user) {
+                return throwError({ message: "User not found", res, status: 404 });
+            }
+
+            return res.json(user.notes);
+        }
+        const users = await User.find();
+        if (!users || users.length === 0) {
+            return res.status(404).json({ message: "No users in the database" });
+        }
+
+        const allNotes = users.map(user => ({
+            userId: user._id,
+            notes: user.notes
+        }));
+
+
+        res.status(200).json({message: "Retrieving notes successfully", notes: allNotes });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Error retrieving notes", error });
+    }
+};
