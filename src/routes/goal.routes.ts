7793c414d84708dc72667eb69a76291383955f27
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-27 14:39:35 +0200

    Add getGoalById router

--- src/routes/goal.routes.ts
+++ src/routes/goal.routes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
-import { createGoal, getGoals } from "../controllers/goal.controller";
+import { createGoal, getGoalById, getGoals } from "../controllers/goal.controller";
 
 const router =  express.Router();
 
 router.post("/", authMiddleware, createGoal);
 router.get("/", authMiddleware, getGoals);
+router.get("/goal", authMiddleware, getGoalById);
 
 export default router;
