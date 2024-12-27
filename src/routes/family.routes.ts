4c37edd29451551cc79cbff651482daf31a466f6
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-28 00:00:27 +0200

    Add getFamily router

--- src/routes/family.routes.ts
+++ src/routes/family.routes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
 import { adminMiddleware } from "../middlewares/adminMiddleware";
-import { getAllFamilies } from "../controllers/family.controller";
+import { getAllFamilies, getFamily } from "../controllers/family.controller";
 
 const router =  express.Router();
 
 router.get("/", authMiddleware, adminMiddleware, getAllFamilies);
+router.get("/getFamily", authMiddleware, getFamily);
 
 export default router;
