c20ee0418f994fe2adc56ace408d66b3cdeabe22
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-27 02:45:51 +0200

    Add deleteNote router

--- src/routes/note.routes.ts
+++ src/routes/note.routes.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
-import { createNote, getNotes, updateNote } from "../controllers/note.controller";
+import { createNote, deleteNote, getNotes, updateNote } from "../controllers/note.controller";
 
 const router =  express.Router();
 
 router.post("/", authMiddleware, createNote);
 router.get("/", authMiddleware, getNotes);
 router.put("/", authMiddleware, updateNote);
+router.delete("/", authMiddleware, deleteNote);
 
 export default router;
