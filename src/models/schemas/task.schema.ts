546d7d7084bba01b390dfdb17284d8d297ceaea9
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-27 13:00:57 +0200

    Add task schema

---
+++ src/models/schemas/task.schema.ts
@@ -0,0 +1,14 @@
+import { Schema } from "mongoose";
+import { ITask } from "../../interfaces/ITask";
+
+export const taskSchema = new Schema<ITask>({
+    title: { type: String, required: true },
+    description: { type: String },
+    type: { type: String, required:true, enum: ['personal', 'family'] },
+    isCompleted: { type: Boolean, default: false },
+    completedAt: { type: Date },
+    rewards: {
+        stars: { type: Number, default: 2 },
+        coins: { type: Number, default: 1 }
+    },
+}, { _id: false });
