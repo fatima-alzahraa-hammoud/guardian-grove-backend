099c5af66106efffbfbeca960809448c17132ce2
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-27 14:01:57 +0200

    Add createGoal router

--- src/routes/goal.routes.ts
+++ src/routes/goal.routes.ts
@@ -1,5 +1,9 @@
 import express from "express";
+import { authMiddleware } from "../middlewares/auth.middleware";
+import { createGoal } from "../controllers/goal.controller";
 
 const router =  express.Router();
+
+router.post("/", authMiddleware, createGoal);
 
 export default router;
