43564dc9aa036133fb661db09bcb10af0a7ff22d
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-27 15:10:18 +0200

    Add deleteGoal router

--- src/routes/goal.routes.ts
+++ src/routes/goal.routes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
-import { createGoal, getGoalById, getGoals, updateGoal } from "../controllers/goal.controller";
+import { createGoal, deleteGoal, getGoalById, getGoals, updateGoal } from "../controllers/goal.controller";
 
 const router =  express.Router();
 
@@ -8,5 +8,6 @@ router.post("/", authMiddleware, createGoal);
 router.get("/", authMiddleware, getGoals);
 router.get("/goal", authMiddleware, getGoalById);
 router.put("/", authMiddleware, updateGoal);
+router.delete("/", authMiddleware, deleteGoal);
 
 export default router;
