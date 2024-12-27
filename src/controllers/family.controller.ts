9a187fade6822e4b18d1193fbb1a3694f6ae5b2c
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-27 23:58:23 +0200

    Add getFamily API

--- src/controllers/family.controller.ts
+++ src/controllers/family.controller.ts
@@ -11,8 +11,24 @@ export const getAllFamilies = async (req: Request, res: Response): Promise<void>
             return throwError({ message: "Family not found.", res, status: 404 });
         }
 
-        res.status(200).send({ message:"Retrieving all families", families });
+        res.status(200).send({ message:"Retrieving all families successfully", families });
     } catch (error) {
         return throwError({ message: "Failed to retrieve all families.", res, status: 500 });
+    }
+};
+
+// Get Family by ID
+export const getFamily = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { familyId } = req.body;
+        const family = await Family.findById(familyId);
+        
+        if (!family) {
+            return throwError({ message: "Family not found.", res, status: 404 });
+        }
+
+        res.status(200).send({ message:"Retrieving family successfully", family });
+    } catch (error) {
+        return throwError({ message: "Failed to retrieve family.", res, status: 500 });
     }
 };
