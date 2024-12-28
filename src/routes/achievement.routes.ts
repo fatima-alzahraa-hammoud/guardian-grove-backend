a9b5804471be540d0d7b07348cbd74092771b238
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-29 01:29:08 +0200

    Add getUnLockedAchievements router

--- src/routes/achievement.routes.ts
+++ src/routes/achievement.routes.ts
@@ -1,16 +1,17 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
-import { createAchievement, deleteAchievement, getAchievements, getLockedAchievements, getUserAchievements, unlockAchievement, updateAchievement } from "../controllers/achievement.controller";
+import { createAchievement, deleteAchievement, getAchievements, getLockedAchievements, getUnLockedAchievements, getUserAchievements, unlockAchievement, updateAchievement } from "../controllers/achievement.controller";
 import { adminMiddleware } from "../middlewares/adminMiddleware";
 
 const router =  express.Router();
 
 router.get("/", authMiddleware, getAchievements);
 router.get("/locked", authMiddleware, getLockedAchievements);
-router.get("/unlocked", authMiddleware, getUserAchievements);
+router.get("/user", authMiddleware, getUserAchievements);
 router.post("/unlock", authMiddleware, unlockAchievement);
 router.post("/", authMiddleware, createAchievement);
 router.put("/", authMiddleware, adminMiddleware, updateAchievement);
 router.delete("/", authMiddleware, adminMiddleware, deleteAchievement);
+router.get("/unlocked", authMiddleware, getUnLockedAchievements);
 
 export default router;
