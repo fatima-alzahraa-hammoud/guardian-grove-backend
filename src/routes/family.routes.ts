e0c33eac6ccbfc000e4188f99fb12c151ab720c2
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-29 00:13:30 +0200

    Add createFamilyTasks router

--- src/routes/family.routes.ts
+++ src/routes/family.routes.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
 import { adminMiddleware } from "../middlewares/adminMiddleware";
-import { deleteFamily, deleteFamilyGoal, getAllFamilies, getFamily, getFamilyGoals, getFamilyMembers, updateFamily, updateFamilyGoal } from "../controllers/family.controller";
+import { createFamilyTasks, deleteFamily, deleteFamilyGoal, getAllFamilies, getFamily, getFamilyGoals, getFamilyMembers, updateFamily, updateFamilyGoal } from "../controllers/family.controller";
 
 const router =  express.Router();
 
@@ -13,5 +13,6 @@ router.delete("/", authMiddleware, deleteFamily);
 router.put("/goal", authMiddleware, updateFamilyGoal);
 router.get("/goals", authMiddleware, getFamilyGoals);
 router.delete("/goal", authMiddleware, deleteFamilyGoal);
+router.post("/goal/task", authMiddleware, createFamilyTasks);
 
 export default router;
