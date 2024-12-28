1bc9d354fd791a71511fd47a04d0e746f6428aeb
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-29 00:30:03 +0200

    Add deleteFamilyTasks router

--- src/routes/family.routes.ts
+++ src/routes/family.routes.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
 import { adminMiddleware } from "../middlewares/adminMiddleware";
-import { createFamilyTasks, deleteFamily, deleteFamilyGoal, getAllFamilies, getFamily, getFamilyGoals, getFamilyMembers, updateFamily, updateFamilyGoal, updateFamilyTask } from "../controllers/family.controller";
+import { createFamilyTasks, deleteFamily, deleteFamilyGoal, deleteFamilyTask, getAllFamilies, getFamily, getFamilyGoals, getFamilyMembers, updateFamily, updateFamilyGoal, updateFamilyTask } from "../controllers/family.controller";
 
 const router =  express.Router();
 
@@ -10,10 +10,15 @@ router.get("/getFamily", authMiddleware, getFamily);
 router.get("/getFamilyMembers", authMiddleware, getFamilyMembers);
 router.put("/", authMiddleware, updateFamily);
 router.delete("/", authMiddleware, deleteFamily);
+
+//routes for family goals
 router.put("/goal", authMiddleware, updateFamilyGoal);
 router.get("/goals", authMiddleware, getFamilyGoals);
 router.delete("/goal", authMiddleware, deleteFamilyGoal);
+
+//routes for family tasks
 router.post("/goal/task", authMiddleware, createFamilyTasks);
 router.put("/goal/task", authMiddleware, updateFamilyTask);
+router.delete("/goal/task", authMiddleware, deleteFamilyTask);
 
 export default router;
