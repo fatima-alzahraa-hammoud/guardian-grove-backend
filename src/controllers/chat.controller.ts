0f41ed49b3fc723f0c7117cb4bea3dd64560c2c1
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-28 15:16:15 +0200

    Add sendMessage API

---
+++ src/controllers/chat.controller.ts
@@ -0,0 +1,47 @@
+import { Request, Response } from "express";
+import { Chat } from "../models/chat.model";
+import { throwError } from "../utils/error";
+import { CustomRequest } from "../interfaces/customRequest";
+import { checkId } from "../utils/checkId";
+import { IMessage } from "../interfaces/IMessage";
+
+//API to send messages and save (and create new chat if no chat exists)
+export const sendMessage = async (req: CustomRequest, res: Response) => {
+
+    if(!req.user){
+        return throwError({ message: "Unauthorized", res, status: 401 });
+    }
+
+    const { chatId, sender, message, image, title } = req.body;
+
+    let chat;
+
+    if(chatId){ 
+        if (!checkId({id: chatId, res})) return;
+        chat = await Chat.findOne({ chatId });
+    }
+        
+    // If no id exists, create a new one with the provided title
+    if (!chatId || !chat) {
+        
+        if (!title) {
+        return res.status(400).send({ message: 'Title is required when creating a new chat' });
+        }
+
+        // Create a new chat with a unique chatId and the provided title
+        chat = new Chat({
+            userId: req.user._id,
+            title,
+            messages: [{ sender, message, image, timestamp: new Date() }],
+        });
+
+        await chat.save();
+        return res.status(201).send({ message: 'Chat started and message sent', chat });
+    }    
+
+    // If chat exists, add the new message to the existing chat
+    chat.messages.push({ sender, message, image, timestamp: new Date() } as IMessage);
+    await chat.save();
+
+    res.status(200).send({ message: 'Message sent', chat });
+};
