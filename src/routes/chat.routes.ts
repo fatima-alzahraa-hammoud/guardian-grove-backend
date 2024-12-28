c3d1d5b5d86d17b174e973ea0bbe01face8ba8dc
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-28 17:08:18 +0200

    Add getUserChats router

--- src/routes/chat.routes.ts
+++ src/routes/chat.routes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
-import { sendMessage, startNewChat } from "../controllers/chat.controller";
+import { getUserChats, sendMessage, startNewChat } from "../controllers/chat.controller";
 //import { sendMessage } from "../controllers/chat.controller";
 
 const router =  express.Router();
@@ -8,5 +8,6 @@ const router =  express.Router();
 //router.post("/", authMiddleware, sendMessage);
 router.post("/", authMiddleware, startNewChat);
 router.post("/send", authMiddleware, sendMessage);
+router.get("/getChats", authMiddleware, getUserChats);
 
 export default router;
