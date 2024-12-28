57112bd89c19692655a586f885899dcf465da095
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-28 15:44:37 +0200

    Add startNewChat router

--- src/routes/chat.routes.ts
+++ src/routes/chat.routes.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
+import { startNewChat } from "../controllers/chat.controller";
 //import { sendMessage } from "../controllers/chat.controller";
 
 const router =  express.Router();
 
 //router.post("/", authMiddleware, sendMessage);
+router.post("/", authMiddleware, startNewChat);
 
 export default router;
