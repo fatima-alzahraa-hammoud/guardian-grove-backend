b66b4bf86adfab53d822f3556558a6daed9671d0
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-28 16:30:04 +0200

    Add sendMessage router

--- src/routes/chat.routes.ts
+++ src/routes/chat.routes.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
-import { startNewChat } from "../controllers/chat.controller";
+import { sendMessage, startNewChat } from "../controllers/chat.controller";
 //import { sendMessage } from "../controllers/chat.controller";
 
 const router =  express.Router();
 
 //router.post("/", authMiddleware, sendMessage);
 router.post("/", authMiddleware, startNewChat);
+router.post("/send", authMiddleware, sendMessage);
 
 export default router;
