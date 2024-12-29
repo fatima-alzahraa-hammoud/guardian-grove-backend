9c0aaa25a2da390d88c05005115f07ecba8bb2aa
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-29 02:27:37 +0200

    Add markAllNotificationsAsRead router

--- src/routes/notification.routes.ts
+++ src/routes/notification.routes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
-import {deleteNotification, getNotifications, markNotificationAsDone, sendNotification, sendSharedNotification, updateNotification } from "../controllers/notification.controller";
+import {deleteNotification, getNotifications, markAllNotificationsAsRead, markNotificationAsDone, sendNotification, sendSharedNotification, updateNotification } from "../controllers/notification.controller";
 import { adminMiddleware } from "../middlewares/adminMiddleware";
 
 const router =  express.Router();
@@ -11,5 +11,6 @@ router.delete("/", authMiddleware, deleteNotification);
 router.put("/", authMiddleware, adminMiddleware, updateNotification);
 router.post("/markAsDone", authMiddleware, markNotificationAsDone);
 router.post("/sendShared", authMiddleware, adminMiddleware, sendSharedNotification);
+router.post("/markAllAsRead", authMiddleware, markAllNotificationsAsRead);
 
 export default router;
