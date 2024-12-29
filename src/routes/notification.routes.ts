d5f7d7d3ad747e380d30d6e808221ea607ed7940
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-29 02:42:27 +0200

    Add getNotificationById router

--- src/routes/notification.routes.ts
+++ src/routes/notification.routes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
-import {deleteNotification, getNotifications, markAllNotificationsAsRead, markNotificationAsDone, sendNotification, sendSharedNotification, updateNotification } from "../controllers/notification.controller";
+import {deleteNotification, getNotificationById, getNotifications, markAllNotificationsAsRead, markNotificationAsDone, sendNotification, sendSharedNotification, updateNotification } from "../controllers/notification.controller";
 import { adminMiddleware } from "../middlewares/adminMiddleware";
 
 const router =  express.Router();
@@ -12,5 +12,6 @@ router.put("/", authMiddleware, adminMiddleware, updateNotification);
 router.post("/markAsDone", authMiddleware, markNotificationAsDone);
 router.post("/sendShared", authMiddleware, adminMiddleware, sendSharedNotification);
 router.post("/markAllAsRead", authMiddleware, markAllNotificationsAsRead);
+router.get("/notification", authMiddleware, getNotificationById);
 
 export default router;
