129207fba740afde304a5d72b9993e45275b9fa1
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-30 19:39:17 +0200

    Add uploadBook router

--- src/routes/book.routes.ts
+++ src/routes/book.routes.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import { uploadBook } from "../controllers/book.controller";
 import { bookUploadMiddleware } from "../middlewares/bookUploadMiddleware";
 import { authMiddleware } from "../middlewares/auth.middleware";
 
 const router =  express.Router();
+
+router.post('/upload', authMiddleware, bookUploadMiddleware, uploadBook);
 
 export default router;
