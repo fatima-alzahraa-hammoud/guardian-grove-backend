bb62883dd9d400767bfd1c357d5e1eb8b47a648f
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-30 22:19:42 +0200

    Add deleteBook router

--- src/routes/book.routes.ts
+++ src/routes/book.routes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { getUserBooks, uploadBook } from "../controllers/book.controller";
+import { deleteBook, getUserBooks, uploadBook } from "../controllers/book.controller";
 import { bookUploadMiddleware } from "../middlewares/bookUploadMiddleware";
 import { authMiddleware } from "../middlewares/auth.middleware";
 
@@ -7,5 +7,6 @@ const router =  express.Router();
 
 router.post('/upload', authMiddleware, bookUploadMiddleware, uploadBook);
 router.get('/', authMiddleware, getUserBooks);
+router.delete('/', authMiddleware, deleteBook);
 
 export default router;
