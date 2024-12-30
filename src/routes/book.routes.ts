482843c72ca9f4bb89675ef117e21385c77d381f
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-30 21:43:51 +0200

    Add getUserBooks router

--- src/routes/book.routes.ts
+++ src/routes/book.routes.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { uploadBook } from "../controllers/book.controller";
+import { getUserBooks, uploadBook } from "../controllers/book.controller";
 import { bookUploadMiddleware } from "../middlewares/bookUploadMiddleware";
 import { authMiddleware } from "../middlewares/auth.middleware";
 
 const router =  express.Router();
 
 router.post('/upload', authMiddleware, bookUploadMiddleware, uploadBook);
+router.get('/', authMiddleware, getUserBooks);
 
 export default router;
