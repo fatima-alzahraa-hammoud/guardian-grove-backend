6f4fc0788d06b81992809af72e52935efdfcf7b8
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-30 09:30:56 +0200

    Fix book interface and schema

--- src/interfaces/IBook.ts
+++ src/interfaces/IBook.ts
@@ -4,10 +4,7 @@ export interface IBook extends Document {
     title: string;
     coverImage: string;
     description: string;
-    author:string,
-    fileUrl: string,
-    fileName: string,
-    fileType: string,
-    uploadedAt: Date,
+    author:string;
+    filePath: string;
+    uploadedAt: Date;
 }
-  
--- src/models/schemas/book.schema.ts
+++ src/models/schemas/book.schema.ts
@@ -6,8 +6,6 @@ export const bookSchema: Schema = new Schema<IBook>({
     coverImage: { type: String, required: true },
     description: { type: String, required: true },
     author: { type: String, required: true },
-    fileUrl: { type: String, required: true }, // URL to the uploaded book
-    fileName: { type: String, required: true },
-    fileType: { type: String, required: true }, // For example, 'pdf'
+    filePath: { type: String, required: true },
     uploadedAt: { type: Date, default: Date.now },
 });
