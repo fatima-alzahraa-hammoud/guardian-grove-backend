efa20a1025419b957623c28290a3a443003c47bf
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-31 13:28:53 +0200

    Add deleteDrawing router

--- src/routes/drawing.routes.ts
+++ src/routes/drawing.routes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
-import { createDrawing, getDrawingById, getDrawings } from "../controllers/drawing.controller";
+import { createDrawing, deleteDrawing, getDrawingById, getDrawings } from "../controllers/drawing.controller";
 import { drawingUploadMiddleware } from "../middlewares/drawingUploadMiddleware";
 
 const router =  express.Router();
@@ -8,5 +8,6 @@ const router =  express.Router();
 router.post("/", authMiddleware, drawingUploadMiddleware, createDrawing);
 router.get("/", authMiddleware, getDrawings);
 router.get("/drawing", authMiddleware, getDrawingById);
+router.delete("/", authMiddleware, deleteDrawing);
 
 export default router;
