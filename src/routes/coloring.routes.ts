889912740f06b0de9c4b1895eafecd97f39cbc87
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-31 23:04:49 +0200

    Add updateColoring router

--- src/routes/coloring.routes.ts
+++ src/routes/coloring.routes.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
 import { imageUploadMiddleware } from "../middlewares/imageUploadMiddleware";
-import { createColoring, deleteColoring, getColoringById, getColorings } from "../controllers/coloring.controller";
+import { createColoring, deleteColoring, getColoringById, getColorings, updateColoring } from "../controllers/coloring.controller";
 
 const router =  express.Router();
 
@@ -9,5 +9,6 @@ router.post("/", authMiddleware, imageUploadMiddleware, createColoring);
 router.get("/", authMiddleware, getColorings);
 router.get("/coloring", authMiddleware, getColoringById);
 router.delete("/", authMiddleware, deleteColoring);
+router.put("/:userId/:coloringId", authMiddleware, imageUploadMiddleware, updateColoring);
 
 export default router;
