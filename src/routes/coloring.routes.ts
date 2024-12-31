068a5f6924d741c60e126e02c2df0df6392a8f0b
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-31 22:38:37 +0200

    Add getColorings router

--- src/routes/coloring.routes.ts
+++ src/routes/coloring.routes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
 import { imageUploadMiddleware } from "../middlewares/imageUploadMiddleware";
-import { createColoring } from "../controllers/coloring.controller";
+import { createColoring, getColorings } from "../controllers/coloring.controller";
 
 const router =  express.Router();
 
 router.post("/", authMiddleware, imageUploadMiddleware, createColoring);
+router.get("/", authMiddleware, getColorings);
 
 export default router;
