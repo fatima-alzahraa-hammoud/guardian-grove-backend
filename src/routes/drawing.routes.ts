c0e0647aa7993937e23ef36d4e923cee121f3ea1
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-31 11:12:45 +0200

    Add createDrawing router

--- src/routes/drawing.routes.ts
+++ src/routes/drawing.routes.ts
@@ -1,5 +1,10 @@
 import express from "express";
+import { authMiddleware } from "../middlewares/auth.middleware";
+import { createDrawing } from "../controllers/drawing.controller";
+import { drawingUploadMiddleware } from "../middlewares/drawingUploadMiddleware";
 
 const router =  express.Router();
+
+router.post("/", authMiddleware, drawingUploadMiddleware, createDrawing);
 
 export default router;
