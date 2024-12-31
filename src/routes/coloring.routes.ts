e48a165364dfda526b56425770b7c00eb1b1119d
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-31 22:49:53 +0200

    Add deleteColoring router

--- src/routes/coloring.routes.ts
+++ src/routes/coloring.routes.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
 import { imageUploadMiddleware } from "../middlewares/imageUploadMiddleware";
-import { createColoring, getColoringById, getColorings } from "../controllers/coloring.controller";
+import { createColoring, deleteColoring, getColoringById, getColorings } from "../controllers/coloring.controller";
 
 const router =  express.Router();
 
 router.post("/", authMiddleware, imageUploadMiddleware, createColoring);
 router.get("/", authMiddleware, getColorings);
 router.get("/coloring", authMiddleware, getColoringById);
+router.delete("/", authMiddleware, deleteColoring);
 
 export default router;
