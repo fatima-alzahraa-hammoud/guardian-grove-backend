889bbd34c0fbbffa0419838efc8e512c523cc7d1
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-31 11:14:01 +0200

    Fix drawingUpload middleware

--- src/middlewares/drawingUploadMiddleware.ts
+++ src/middlewares/drawingUploadMiddleware.ts
@@ -4,18 +4,12 @@ import { Request } from 'express';
 const storage = multer.memoryStorage();
 
 const fileFilter = (req: Request, file: Express.Multer.File, cb: FileFilterCallback) => {
-    if (file.fieldname === 'drawing') {
-        const allowedMimeTypes = [
-            'image/jpeg',
-            'image/png',
-            'image/gif'
-        ];
-        if (!allowedMimeTypes.includes(file.mimetype)) {
-            (req as any).fileValidationError = 'Only JPEG, PNG, and GIF files are allowed for coverImage';
-            return cb(null, false);
-        }
+    const allowedMimeTypes = ['image/jpeg', 'image/png', 'image/gif'];
+    if (file.fieldname === 'drawing' && !allowedMimeTypes.includes(file.mimetype)) {
+        (req as any).fileValidationError = 'Only JPEG, PNG, and GIF files are allowed for drawings';
+        return cb(null, false);
     }
-
+    
     cb(null, true);
 };
 
