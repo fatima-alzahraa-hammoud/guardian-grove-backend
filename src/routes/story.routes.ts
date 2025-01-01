871e858e8e8f0ffda0887172a8bffe6c1afda404
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-01 13:24:55 +0200

    Add getStories router

--- src/routes/story.routes.ts
+++ src/routes/story.routes.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
-import { createStory } from "../controllers/story.controller";
+import { createStory, getStories } from "../controllers/story.controller";
 
 const router =  express.Router();
 
 router.post("/", authMiddleware, createStory);
+router.get("/", authMiddleware, getStories);
 
 export default router;
