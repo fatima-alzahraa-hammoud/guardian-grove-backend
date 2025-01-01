4c548fec0e8703235289fbb380743a870313d992
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-01 13:30:37 +0200

    Add getStoryById router

--- src/routes/story.routes.ts
+++ src/routes/story.routes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
-import { createStory, getStories } from "../controllers/story.controller";
+import { createStory, getStories, getStoryById } from "../controllers/story.controller";
 
 const router =  express.Router();
 
 router.post("/", authMiddleware, createStory);
 router.get("/", authMiddleware, getStories);
+router.get("/story/:storyId", authMiddleware, getStoryById);
 
 export default router;
