12dbf2e1efedb98119d6cdeae6367ecc510e90c4
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-01 13:39:22 +0200

    Add deleteStory router

--- src/routes/story.routes.ts
+++ src/routes/story.routes.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
-import { createStory, getStories, getStoryById } from "../controllers/story.controller";
+import { createStory, deleteStory, getStories, getStoryById } from "../controllers/story.controller";
 
 const router =  express.Router();
 
 router.post("/", authMiddleware, createStory);
 router.get("/", authMiddleware, getStories);
 router.get("/story/:storyId", authMiddleware, getStoryById);
+router.delete("/", authMiddleware, deleteStory);
 
 export default router;
