2fe35b8d577e02df88b0224428f453232ad6d9eb
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-09 14:15:06 +0200

    Add getMonthlyStats router

--- src/routes/goal.routes.ts
+++ src/routes/goal.routes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
-import { completeTask, createGoal, createUserTask, deleteGoal, deleteTask, getGoalById, getGoals, getTaskById, updateTask, updateUserGoal } from "../controllers/goal.controller";
+import { completeTask, createGoal, createUserTask, deleteGoal, deleteTask, getGoalById, getGoals, getMonthlyStats, getTaskById, updateTask, updateUserGoal } from "../controllers/goal.controller";
 
 const router =  express.Router();
 
@@ -17,5 +17,7 @@ router.get("/task", authMiddleware, getTaskById);
 router.put("/task", authMiddleware, updateTask);
 router.delete("/task", authMiddleware, deleteTask);
 router.post("/taskDone", authMiddleware, completeTask);
+
+router.get("/monthlyStats", authMiddleware, getMonthlyStats);
 
 export default router;
