9bc70239816cbf208c71e60cddbc1267e7e891d3
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-06-21 18:01:51 +0300

    Fix jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
+// jest.config.js - Updated to use SWC instead of ts-jest
 module.exports = {
-  preset: 'ts-jest',
+  // Remove ts-jest preset
+  // preset: 'ts-jest', // REMOVE THIS LINE
+  
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.ts'],
   roots: ['<rootDir>/src', '<rootDir>/tests'],
@@ -12,10 +15,57 @@ module.exports = {
   coverageReporters: ['text', 'lcov', 'html'],
   setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  
+  // NEW: Use SWC for much faster TypeScript compilation
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest'
+    '^.+\\.(ts|tsx)$': ['@swc/jest', {
+      jsc: {
+        parser: {
+          syntax: 'typescript',
+          tsx: false,
+          decorators: true,
+        },
+        target: 'es2020',
+        keepClassNames: true,
+        transform: {
+          legacyDecorator: true,
+          decoratorMetadata: true,
+        },
+      },
+      module: {
+        type: 'commonjs',
+      },
+    }],
   },
-  testTimeout: 10000,
+  
+  // Performance optimizations
+  maxWorkers: '100%',
+  testTimeout: 5000,
+  cache: true,
+  cacheDirectory: '<rootDir>/.jest-cache',
+  collectCoverage: process.env.CI === 'true',
+  
+  // SWC is so fast, we can remove some ts-jest specific optimizations
+  // Remove the globals section entirely since we're not using ts-jest
+  
   clearMocks: true,
-  restoreMocks: true
+  restoreMocks: true,
+  resetMocks: false,
+  watchman: true,
+  workerIdleMemoryLimit: '512MB',
+  
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/dist/',
+    '<rootDir>/build/'
+  ],
+  
+  modulePathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/build/'],
+  
+  watchPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/dist/',
+    '<rootDir>/coverage/',
+    '<rootDir>/.jest-cache/'
+  ]
 };
