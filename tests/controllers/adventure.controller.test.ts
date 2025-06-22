8776bd8c0f722e0e2053197dce80c127fb99b255
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-06-22 13:12:31 +0300

    Fix setup adventure controller test

--- tests/controllers/adventure.controller.test.ts
+++ tests/controllers/adventure.controller.test.ts
@@ -1,11 +1,9 @@
 import { Adventure } from '../../src/models/adventure.model';
 import * as checkId from '../../src/utils/checkId';
-import * as errorUtils from '../../src/utils/error';
 
 // Mock all dependencies
 jest.mock('../../src/models/adventure.model');
 jest.mock('../../src/utils/checkId');
-jest.mock('../../src/utils/error');
 
 const mockAdventure = Adventure as jest.Mocked<typeof Adventure>;
 const mockCheckId = checkId as jest.Mocked<typeof checkId>;
@@ -14,7 +12,7 @@ describe('Adventure Controller Tests', () => {
     beforeEach(() => {
         jest.clearAllMocks();
         
-        // Setup model methods
+        // Setup Adventure model methods
         mockAdventure.find = jest.fn();
         mockAdventure.findById = jest.fn();
         mockAdventure.findByIdAndUpdate = jest.fn();
@@ -23,5 +21,4 @@ describe('Adventure Controller Tests', () => {
         // Setup utility mocks
         mockCheckId.checkId.mockReturnValue(true);
     });
-
 });
