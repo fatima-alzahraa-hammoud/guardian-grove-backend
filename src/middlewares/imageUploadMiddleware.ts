7180f0d5afb679ce634bea4b2b131636089ade92
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-06-29 16:50:54 +0300

    Fix imageUploadMiddleware to handle FamilyAvatar

--- src/middlewares/imageUploadMiddleware.ts
+++ src/middlewares/imageUploadMiddleware.ts
@@ -5,7 +5,7 @@ const storage = multer.memoryStorage();
 
 const fileFilter = (req: Request, file: Express.Multer.File, cb: FileFilterCallback) => {
     const allowedMimeTypes = ['image/jpeg', 'image/png', 'image/gif'];
-    if ((file.fieldname === 'imageUrl' || file.fieldname === 'avatar') && !allowedMimeTypes.includes(file.mimetype)) {
+    if ((file.fieldname === 'imageUrl' || file.fieldname === 'avatar' || file.fieldname === 'familyAvatar') && !allowedMimeTypes.includes(file.mimetype)) {
         (req as any).fileValidationError = 'Only JPEG, PNG, and GIF files are allowed for images';
         return cb(null, false);
     }
@@ -19,4 +19,5 @@ const upload = multer({ storage, fileFilter });
 export const imageUploadMiddleware = upload.fields([
     { name: 'imageUrl', maxCount: 1 },
     { name: 'avatar', maxCount: 1 },
+    { name: 'familyAvatar', maxCount: 1 },
 ]);
