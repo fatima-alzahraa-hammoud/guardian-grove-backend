603ab64fa9d903a8fbaee5940979a57b653e03c3
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-06-29 20:26:03 +0300

    Add imageUploadMiddleware to register router

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { forgetPassword, login, register } from "../controllers/auth.controller";
+import { imageUploadMiddleware } from "../middlewares/imageUploadMiddleware";
 
 const router =  express.Router();
 
 router.post("/login", login);
-router.post("/register", register);
+router.post("/register", imageUploadMiddleware, register);
 router.post("/forget-password", forgetPassword); 
 
 export default router;
