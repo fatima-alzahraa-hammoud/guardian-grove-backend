6ed54a6743789a15484dbfeb5faa41996e729491
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-06-29 16:50:09 +0300

    Fix sanitizePublicId helper function

--- src/utils/sanitizePublicId.ts
+++ src/utils/sanitizePublicId.ts
@@ -1,5 +1,5 @@
 //function to set the public id
 
 export const sanitizePublicId = (filename: string): string => {
-    return filename.replace(/[^a-zA-Z0-9-_أ-ي]/g, '_');
+    return filename.replace(/[^a-zA-Z0-9-_أ-ي]/g, '_').replace(/_{2,}/g, '_');
 };
