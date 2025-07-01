8a397e6ba816813f13f223e96cb70c11be96969f
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-07-02 00:47:41 +0300

    Add incrementDownloads route

--- src/routes/bondingActivity.routes.ts
+++ src/routes/bondingActivity.routes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
-import { createBondingActivity, deleteBondingActivity, getBondingActivities, updateBondingActivity,} from "../controllers/bondingActivity.controller";
+import { createBondingActivity, deleteBondingActivity, getBondingActivities, incrementDownloads, updateBondingActivity,} from "../controllers/bondingActivity.controller";
 
 const router = express.Router();
 
@@ -10,5 +10,6 @@ router.post("/", createBondingActivity);
 router.get("/", getBondingActivities);
 router.put("/:activityId", updateBondingActivity);
 router.delete("/:activityId", deleteBondingActivity);
+router.patch("/:activityId/download", incrementDownloads);
 
 export default router;
