787fcbacec7aa7a5b784e1b39f492b3184b333ba
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-07-01 22:54:11 +0300

    Add createJournalEntry route

--- src/routes/journal.routes.ts
+++ src/routes/journal.routes.ts
@@ -1,3 +1,13 @@
 import express from "express";
+import { authMiddleware } from "../middlewares/auth.middleware";
+import { journalUploadMiddleware } from "../middlewares/journal.middleware";
+import { createJournalEntry } from "../controllers/journal.controller";
 
 const router =  express.Router();
+
+router.use(authMiddleware);
+
+// Create a new journal entry
+router.post("/", journalUploadMiddleware, createJournalEntry);
+
+export default router;
