0ac6319b53c932cd34efca7ca82f6890168ebaeb
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-07-02 00:46:40 +0300

    Add getBonding Activities route

--- src/routes/bondingActivity.routes.ts
+++ src/routes/bondingActivity.routes.ts
@@ -1,13 +1,12 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
-import { 
-    createBondingActivity,} from "../controllers/bondingActivity.controller";
+import { createBondingActivity, getBondingActivities,} from "../controllers/bondingActivity.controller";
 
 const router = express.Router();
 
 router.use(authMiddleware);
 
 router.post("/", createBondingActivity);
-
+router.get("/", getBondingActivities);
 
 export default router;
