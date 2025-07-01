ec86ca5dcb098be03be2837e79759757c5cd1dfe
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-07-02 00:47:23 +0300

    Add deleteBondingActivity route

--- src/routes/bondingActivity.routes.ts
+++ src/routes/bondingActivity.routes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
-import { createBondingActivity, getBondingActivities, updateBondingActivity,} from "../controllers/bondingActivity.controller";
+import { createBondingActivity, deleteBondingActivity, getBondingActivities, updateBondingActivity,} from "../controllers/bondingActivity.controller";
 
 const router = express.Router();
 
@@ -9,5 +9,6 @@ router.use(authMiddleware);
 router.post("/", createBondingActivity);
 router.get("/", getBondingActivities);
 router.put("/:activityId", updateBondingActivity);
+router.delete("/:activityId", deleteBondingActivity);
 
 export default router;
