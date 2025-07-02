38249ad190d9fca2eebd78570714f6f77ea44ea3
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-07-02 11:48:06 +0300

    Fix family model

--- src/models/family.model.ts
+++ src/models/family.model.ts
@@ -6,6 +6,7 @@ import { notificationSchema } from "./schemas/notification.schema";
 import { unlockedAchievementSchema } from "./schemas/unlockedAchievementSchema.schema";
 import { storySchema } from "./schemas/story.schema";
 import { journalEntrySchema } from "./schemas/journalEntry.schema";
+import { bondingEventSchema } from "./schemas/bondingEventSchema";
 
 const familySchema = new Schema<IFamily>({
     _id: { type: Schema.Types.ObjectId, auto: true },
@@ -32,6 +33,7 @@ const familySchema = new Schema<IFamily>({
     familyAvatar: { type: String, required: true },
     sharedStories: { type: [storySchema], default: [] },
     journalEntries: { type: [journalEntrySchema], default: [] },
+    bondingEvents: [bondingEventSchema],
 });
 
 // virtual field to get members dynamically
